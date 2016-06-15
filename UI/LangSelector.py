b1b90c736745b40842f7168245888038ead5b012
Xavier HD <xavierhd@hotmail.com>
2016-06-15 17:37:20 -0400

    Changed the name of the language file

--- UI/LangSelector.py
+++ UI/LangSelector.py
@@ -3,5 +3,5 @@
 from Utils import FileReader
 
 def getLang(language):
-    langfile = "./UI/lang/{0}.yaml".format(language)
+    langfile = "./UI/locale/{0}.yaml".format(language)
     return FileReader.readYaml(langfile)
