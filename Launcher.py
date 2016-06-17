177b0ab860e2174bdbe7da0a356ec6c4c4d02bf2
Xavier HD <xavierhd@hotmail.com>
2016-06-17 13:31:10 -0400

    Network related folder

--- Launcher.py
+++ Launcher.py
@@ -37,20 +37,5 @@ class Commander(object):
     def callBack(self, args):
         pass
 
-    def internetExist(self):
-        r = requests.head("http://google.ca")
-        return str(r.status_code) == '301'
-
-
-
-    def readIP(self, path):
-        ip = None
-        try:
-            with open(path) as file:
-                ip = f.readline()
-        except:
-            print ("no file named %s" %path)
-        return ip
-
 if __name__ == '__main__':
     Commander()
