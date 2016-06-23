f6b2d0f46005db0ecf3031fcc080e6db624c0c57
xavierhd <xavierhd@hotmail.com>
2016-06-23 14:44:10 -0400

    Network stuff in network class, readme and ignore updated

---
+++ Utils/Network.py
@@ -0,0 +1,15 @@
+    
+def internetExist(self):
+    r = requests.head("http://google.ca")
+    return str(r.status_code) == '301'
+
+
+
+def readIP(self, path):
+    ip = None
+    try:
+        with open(path) as file:
+            ip = f.readline()
+    except:
+        print ("no file named %s" %path)
+    return ip
